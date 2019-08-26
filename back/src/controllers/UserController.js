a58bac371e871537b18efd2aa4790b99b5bd2708
XakimAA <malutok@mail.ru>
2019-08-26 20:39:02 +0500

    user's function

--- back/src/controllers/UserController.js
+++ back/src/controllers/UserController.js
@@ -5,17 +5,6 @@ class UserController {
     constructor(){
         const p = 123;
     };
-    index(req,res){
-        const id = req.params.id;
-        UserModel.findById(id,(err, user) => {
-            if (err) {
-                return res.status(404).json({
-                    message: "Not found"
-                });
-            }
-            res.json(user);
-        });
-    }
 
     create(req, res) {
         const postData = {
@@ -31,22 +20,66 @@ class UserController {
           res.json(reason);
         });
     }
+    
+    index(req,res){
+        const id = req.params.id;
+        UserModel.findById(id,(err, user) => {
+            if (err) {
+                return res.status(404).json({
+                    message: "Not found"
+                });
+            }
+            res.json(user);
+        });
+    }
+
+    getAll(req,res){
+        UserModel.find().then((err,users) => {
+            if (err){
+                return res.send(err);
+            }
+            res.json(users);
+        });
+    }
+
+    update(req,res){
+        UserModel.findByIdAndUpdate(req.params.id, {$set:req.body},(err)=>{
+            if (err)
+                return res.send(err);
+            else 
+                res.json({message:"User updated"});
+        })
+    }
+
 
     delete(req, res) {
-        const id = req.params.id;
-        UserModel.findOneAndRemove (id)
-        .then(user=>{
+        // const id = req.params.id;
+        // UserModel.findOneAndRemove (id)
+        // .then(user=>{
+        //     if (user) {
+        //         res.json({
+        //             message: "User deleted"
+        //         });
+        //     }
+        // })
+        // .catch(err =>{
+        //     res.json({
+        //         message: "User not found"
+        //     });
+        // });
+        UserModel.remove({
+            _id: req.params.id
+        }).then(user => {
             if (user) {
                 res.json({
                     message: "User deleted"
-                });
+                })
+            } else {
+                res.json({
+                    message: "User is not found"
+                })
             }
         })
-        .catch(err =>{
-            res.json({
-                message: "User not found"
-            });
-        });
     }
 }
 
--- back/src/index.js
+++ back/src/index.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import cors from 'cors'
 import UserModel from "./schemas";
 //import UserController from "./controllers";
 import {UserController} from './controllers/UserController';
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const User = new UserController();
@@ -18,10 +19,12 @@ mongoose.connect('mongodb://localhost:27017/football', {
   useFindAndModify:true
 });
 
-app.get('/user/:id', User.index);
-app.post('/user/create', User.create);
-app.delete('/user/:id', User.delete);
+app.get('/users/:id', User.index);
+app.post('/users/create', User.create);
+app.delete('/users/:id', User.delete);
+app.get('/users', User.getAll);
+app.put('/users/:id',User.update)
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(9999, function () {
+  console.log('Example app listening on port 9999!');
 });
